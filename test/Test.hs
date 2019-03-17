1e75a477839033ddbb3d17ee4e6a774b07428c9e
mark on github <markongithub@users.noreply.github.com>
2019-03-17 23:12:38 +0000

    Now with tests

--- test/Test.hs
+++ test/Test.hs
@@ -1,12 +1,43 @@
 module Main where
 
+import qualified Data.Map as Map
 import Test.Framework (defaultMain)
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit (assertEqual)
 
 import SLCSP
 
-oneTest = testCase "please pass" $ assertEqual "whatever" 1 1
+testZipCodes =  [ ZipCode "00000" "NJ" "1"
+                , ZipCode "11111" "VA" "2"
+                , ZipCode "22222" "NH" "3"
+                ]
+zipTable = rateAreasByZip testZipCodes
 
-main = defaultMain [oneTest]
+testPlans = [ Plan "NJ" "Double Platinum" 99.99  "1"
+            , Plan "NJ" "Silver"          191.18 "1"
+            , Plan "NJ" "Silver"          200.00 "1"
+            , Plan "VA" "Silver"          200.00 "2"
+            ]
+rateTable = silverPlansByRateArea testPlans
+
+equalityTest description expected actual =
+  testCase description $ assertEqual description expected actual
+
+testOnlyOneSilverPlan = equalityTest "testOnlyOneSilverPlan" ""
+                          $ formattedSLCSP zipTable rateTable "11111"
+
+testNoRateAreaForZIP = equalityTest "testNoRateAreaForZIP" ""
+                         $ formattedSLCSP zipTable rateTable "33333"
+
+testNoPlansForRateArea = equalityTest "testNoPlansForRateArea" ""
+                          $ formattedSLCSP zipTable rateTable "22222"
+
+testCorrectSLCSP = equalityTest "testCorrectSLCSP" "200.00"
+                     $ formattedSLCSP zipTable rateTable "00000"
+
+main = defaultMain [ testOnlyOneSilverPlan
+                   , testNoRateAreaForZIP
+                   , testNoPlansForRateArea
+                   , testCorrectSLCSP
+                   ]
 
