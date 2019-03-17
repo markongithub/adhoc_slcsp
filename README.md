c07c803e64d54d7ff02fe28dd39dd7f73444aa59
mark on github <markongithub@users.noreply.github.com>
2019-03-17 23:46:50 +0000

    Now with command-line arguments

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 This package provides a solution in Haskell to [Ad Hoc's Second-Lowest Cost Silver Plan](https://homework.adhoc.team/slcsp/) problem.
 
-After downloading it, place the necessary input files in the `./input` subdirectory and run
+After downloading it, you can run
 - cabal install --enable-tests
 - cabal test (optional)
 - cabal run
+
+By default we assume the three input files specified in the assignment are in
+the `./input` subdirectory of the working directory. However, the user can
+override this default with the command-line parameters `--planfile`,
+`--zipfile`, and `--requestfile`. For example one could run
+> `cabal run -- --zipfile /tmp/lots_of_zip_codes.csv`
--- src/Main.hs
+++ src/Main.hs
@@ -3,13 +3,26 @@ module Main where
 import qualified Data.ByteString.Lazy.Char8 as Char8
 import qualified Data.Csv as CSV
 
+import System.Console.CmdArgs
 import SLCSP
+
+data Options = Options {
+    planFile :: String
+  , zipFile :: String
+  , requestFile :: String
+} deriving (Data, Typeable)
+
+options = Options { planFile = "input/plans.csv"
+                  , zipFile = "input/zips.csv"
+                  , requestFile = "input/slcsp.csv"
+                  }
 
 main :: IO ()
 main = do
-  plans <- plansFromFile "input/plans.csv"
-  zips <- zipsFromFile "input/zips.csv"
-  requests <- requestsFromFile "input/slcsp.csv"
+  opts <- cmdArgs options
+  plans <- plansFromFile (planFile opts)
+  zips <- zipsFromFile (zipFile opts)
+  requests <- requestsFromFile (requestFile opts)
   let zipTable = rateAreasByZip zips
   let rateTable = silverPlansByRateArea plans
   let finalPlans = map (lookupRequest zipTable rateTable) requests
